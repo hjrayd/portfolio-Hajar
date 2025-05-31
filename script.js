f04783ce3955d7fdc44168ca771480d52a932526
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2025-05-31 15:19:55 +0200

    transition js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+  const fadeEls = document.querySelectorAll('.fade-in');
+  fadeEls.forEach(el => {
+    el.classList.add('visible');
+  });
+});
